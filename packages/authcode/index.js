1eb3c7b6b867a21977ed2279cd7f18278359e527
moselikk <moselikk@gmail.com>
2022-08-27 20:47:13 +0800

    feat: add authcode element

---
+++ packages/authcode/index.js
@@ -0,0 +1,9 @@
+
+import TdsAuthCode from './src';
+
+/* istanbul ignore next */
+TdsAuthCode.install = function(Vue) {
+  Vue.component(TdsAuthCode.name, TdsAuthCode);
+};
+
+export default TdsAuthCode;
---
+++ packages/authcode/src/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <h1>{{title ? title : '请输入验证码'}}</h1>
+    <div ref="inputList">
+      <input type="text" maxlength="1" autofocus="autofocus" v-for="(item,index) in code" :key="index" v-model.number="code[index]" @keyup="handleKey(index)" />
+    </div>
+    <br />
+    <button @click="handleSubmit">提交</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TdsAuthCode',
+  props:{
+    title: String,
+    length:{
+      type: String,
+      default: '4'
+    } 
+  },
+  data(){
+    return{
+      code: []
+    }
+  },
+  created(){
+    const length = Number(this.length)
+    this.code = new Array(length)
+  },
+  methods: {
+    handleSubmit(){
+      // 处理数据，发请求
+      alert('提交成功')
+    },
+    handleKey(index){
+      const inputNodes = this.$refs.inputList.childNodes
+      if(index==(inputNodes.length-1)){
+        inputNodes[index].blur()
+      }
+      if(inputNodes[index].value){
+        if(index<(inputNodes.length-1)){
+          inputNodes[index+1].focus()
+        }
+      }else if(!inputNodes[index].value && index>0){
+        inputNodes[index-1].focus()
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+input{
+  height: 50px;
+  width: 30px;
+  line-height: 40px;
+  font-size: 40px;
+  margin-left: 15px;
+  text-align: center;
+}
+input:nth-child(1){
+  margin-left: 0;
+}
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none !important;
+}
+input[type='number'] {
+    -moz-appearance: textfield !important;
+}
+</style>
